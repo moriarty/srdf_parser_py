9b6aa4ae954c437452fc43b96759380a7aef0cab
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2015-02-03 18:46:16 +0100

    Removed reference to xml_reflection

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['srdf_parser_py', 'srdf_parser_py.xml_reflection'],
+    packages=['srdf_parser_py'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
